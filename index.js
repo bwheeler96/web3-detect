6a89aa877fe2d05a00052fc66f93a37bd6460ffe
Brian Wheeler <brianwheeler@Brians-MacBook-Pro.local>
2018-09-27 15:10:28 -0700

    init

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const { promisifyAll } = require('bluebird')
+
+module.exports.detect = function() {
+    if (typeof window.web3 === undefined) {
+        return Promise.resolve({
+            walletAvailable: false
+        })
+    } else {
+        const { web3 } = window
+
+        let response = {
+            walletAvailable: true
+        }
+
+        return new Promise((resolve, reject) => {
+            web3.version.getNetwork((err, networkId) => {
+                if (err)
+                    reject(err)
+                else {
+                    response.networkId = networkId
+                    resolve()
+                }
+            })
+        })
+        .then(() => {
+            web3.eth.getAccounts((err, accounts) => {
+                if (err)
+                    reject(err)
+                else {
+                    response.accounts = accounts
+                    if (accounts.length === 0) {
+                        response.walletUnlocked = false
+                    } else {
+                        response.walletUnlocked = true
+                    }
+                    resolve(response)
+                }
+            })
+        })
+    }
+}
